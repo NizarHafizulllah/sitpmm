90bb6ce32a7259dfb442ea32394ac07b71a5f51e
Rezkhadhitya <darkroomeffect@gmail.com>
2019-11-24 02:37:05 +0700

    database

--- database/sitpmm.sql
+++ database/sitpmm.sql
@@ -1,24 +1,27 @@
-/*
-Navicat MySQL Data Transfer
+-- MySQL dump 10.17  Distrib 10.3.16-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: sitpmm
+-- ------------------------------------------------------
+-- Server version	10.3.16-MariaDB
 
-Source Server         : localhost
-Source Server Version : 50505
-Source Host           : 127.0.0.1:3306
-Source Database       : sitpmm
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-Target Server Type    : MYSQL
-Target Server Version : 50505
-File Encoding         : 65001
+--
+-- Table structure for table `barang`
+--
 
-Date: 2019-11-22 05:53:17
-*/
-
-SET FOREIGN_KEY_CHECKS=0;
-
--- ----------------------------
--- Table structure for barang
--- ----------------------------
 DROP TABLE IF EXISTS `barang`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `barang` (
   `id` int(11) NOT NULL,
   `kategori` int(11) DEFAULT NULL,
@@ -33,29 +36,47 @@ CREATE TABLE `barang` (
   `request` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of barang
--- ----------------------------
+--
+-- Dumping data for table `barang`
+--
 
--- ----------------------------
--- Table structure for m_kategori
--- ----------------------------
+LOCK TABLES `barang` WRITE;
+/*!40000 ALTER TABLE `barang` DISABLE KEYS */;
+/*!40000 ALTER TABLE `barang` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `m_kategori`
+--
+
 DROP TABLE IF EXISTS `m_kategori`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `m_kategori` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `kategori` varchar(150) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of m_kategori
--- ----------------------------
+--
+-- Dumping data for table `m_kategori`
+--
 
--- ----------------------------
--- Table structure for m_menu
--- ----------------------------
+LOCK TABLES `m_kategori` WRITE;
+/*!40000 ALTER TABLE `m_kategori` DISABLE KEYS */;
+/*!40000 ALTER TABLE `m_kategori` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `m_menu`
+--
+
 DROP TABLE IF EXISTS `m_menu`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `m_menu` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `menu` varchar(50) DEFAULT NULL,
@@ -63,97 +84,76 @@ CREATE TABLE `m_menu` (
   `url` varchar(50) DEFAULT NULL,
   `icon` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of m_menu
--- ----------------------------
-INSERT INTO `m_menu` VALUES ('1', 'PENGADAAN BARANG', 'REQUEST', 'PermintaanBarang', 'fas fa-fw fa-sticky-note');
-INSERT INTO `m_menu` VALUES ('2', 'PENGADAAN BARANG', 'APRV LVL 1', 'Penyetujuan1', 'fas fa-fw fa-sticky-note');
-INSERT INTO `m_menu` VALUES ('3', 'PENGADAAN BARANG', 'APRV LVL 2', 'Penyetujuan2', 'fas fa-fw fa-sticky-note');
-INSERT INTO `m_menu` VALUES ('4', 'PENGADAAN BARANG', 'REF BARANG', 'ReferensiBarang', 'fas fa-fw fa-sticky-note');
-INSERT INTO `m_menu` VALUES ('5', 'PENGADAAN BARANG', 'APRV LVL 3', 'PenyetujuanLevel3', 'fas fa-fw fa-sticky-note');
-INSERT INTO `m_menu` VALUES ('6', 'PENGADAAN BARANG', 'PAYMENT', 'Pembayaran', 'fas fa-fw fa-sticky-note');
-INSERT INTO `m_menu` VALUES ('7', 'PENGADAAN BARANG', 'BARANG MASUK', 'BarangMasuk', 'fas fa-fw fa-sticky-note');
-INSERT INTO `m_menu` VALUES ('8', 'AKUN', 'PROFILE', 'Profile', 'fas fa-fw fa-user');
-INSERT INTO `m_menu` VALUES ('9', 'AKUN', 'LOGOUT', 'Login/logout', 'fas fa-fw fa-user');
+--
+-- Dumping data for table `m_menu`
+--
 
--- ----------------------------
--- Table structure for temp_barang
--- ----------------------------
-DROP TABLE IF EXISTS `temp_barang`;
-CREATE TABLE `temp_barang` (
-  `id` int(11) NOT NULL,
-  `session_id` int(11) DEFAULT NULL,
-  `kategori` int(11) DEFAULT NULL,
-  `merk` varchar(255) DEFAULT NULL,
-  `suplier` int(11) DEFAULT NULL,
-  `nama_barang` varchar(100) DEFAULT NULL,
-  `harga_satuan` float(11,0) DEFAULT NULL,
-  `qty` int(11) DEFAULT NULL,
-  `keterangan` text DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+LOCK TABLES `m_menu` WRITE;
+/*!40000 ALTER TABLE `m_menu` DISABLE KEYS */;
+INSERT INTO `m_menu` VALUES (1,'PENGADAAN BARANG','REQUEST','PermintaanBarang','fas fa-fw fa-sticky-note'),(2,'PENGADAAN BARANG','APRV LVL 1','Penyetujuan1','fas fa-fw fa-sticky-note'),(3,'PENGADAAN BARANG','APRV LVL 2','Penyetujuan2','fas fa-fw fa-sticky-note'),(4,'PENGADAAN BARANG','REF BARANG','ReferensiBarang','fas fa-fw fa-sticky-note'),(5,'PENGADAAN BARANG','APRV LVL 3','PenyetujuanLevel3','fas fa-fw fa-sticky-note'),(6,'PENGADAAN BARANG','PAYMENT','Pembayaran','fas fa-fw fa-sticky-note'),(7,'PENGADAAN BARANG','BARANG MASUK','BarangMasuk','fas fa-fw fa-sticky-note'),(8,'AKUN','PROFILE','Profile','fas fa-fw fa-user'),(9,'AKUN','LOGOUT','Login/logout','fas fa-fw fa-user'),(10,'MASTER','BARANG','Barang','fas fa-fw fa-sticky-note'),(11,'MASTER','PEGAWAI','Pegawai','fas fa-fw fa-user'),(12,'MASTER','DIVISI','Divisi','fas fa-fw fa-landmark');
+/*!40000 ALTER TABLE `m_menu` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- ----------------------------
--- Records of temp_barang
--- ----------------------------
+--
+-- Table structure for table `t_akses`
+--
 
--- ----------------------------
--- Table structure for t_akses
--- ----------------------------
 DROP TABLE IF EXISTS `t_akses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `t_akses` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_menu` int(11) NOT NULL,
   `id_role` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of t_akses
--- ----------------------------
-INSERT INTO `t_akses` VALUES ('1', '1', '1');
-INSERT INTO `t_akses` VALUES ('8', '8', '1');
-INSERT INTO `t_akses` VALUES ('9', '9', '1');
-INSERT INTO `t_akses` VALUES ('10', '2', '1');
-INSERT INTO `t_akses` VALUES ('11', '3', '1');
-INSERT INTO `t_akses` VALUES ('12', '4', '1');
-INSERT INTO `t_akses` VALUES ('13', '5', '1');
-INSERT INTO `t_akses` VALUES ('14', '6', '1');
-INSERT INTO `t_akses` VALUES ('15', '7', '1');
-INSERT INTO `t_akses` VALUES ('16', '1', '2');
-INSERT INTO `t_akses` VALUES ('17', '2', '0');
-INSERT INTO `t_akses` VALUES ('18', '3', '0');
-INSERT INTO `t_akses` VALUES ('19', '4', '0');
-INSERT INTO `t_akses` VALUES ('20', '5', '2');
-INSERT INTO `t_akses` VALUES ('21', '6', '0');
-INSERT INTO `t_akses` VALUES ('22', '7', '0');
-INSERT INTO `t_akses` VALUES ('23', '8', '2');
-INSERT INTO `t_akses` VALUES ('24', '9', '2');
+--
+-- Dumping data for table `t_akses`
+--
 
--- ----------------------------
--- Table structure for t_divisi
--- ----------------------------
+LOCK TABLES `t_akses` WRITE;
+/*!40000 ALTER TABLE `t_akses` DISABLE KEYS */;
+INSERT INTO `t_akses` VALUES (1,1,1),(8,8,1),(9,9,1),(10,2,1),(11,3,1),(12,4,1),(13,5,1),(14,6,1),(15,7,1),(16,1,2),(17,2,0),(18,3,0),(19,4,0),(20,5,2),(21,6,0),(22,7,0),(23,8,2),(24,9,2),(25,10,1),(26,11,1),(27,12,1);
+/*!40000 ALTER TABLE `t_akses` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `t_divisi`
+--
+
 DROP TABLE IF EXISTS `t_divisi`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `t_divisi` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `divisi` varchar(100) DEFAULT NULL,
   `kepala_divisi` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of t_divisi
--- ----------------------------
-INSERT INTO `t_divisi` VALUES ('1', 'DIVISI IT', null);
-INSERT INTO `t_divisi` VALUES ('2', 'DIVISI FINANCE', null);
-INSERT INTO `t_divisi` VALUES ('3', 'DIVISI LAIN-LAIN', null);
+--
+-- Dumping data for table `t_divisi`
+--
 
--- ----------------------------
--- Table structure for t_pegawai
--- ----------------------------
+LOCK TABLES `t_divisi` WRITE;
+/*!40000 ALTER TABLE `t_divisi` DISABLE KEYS */;
+INSERT INTO `t_divisi` VALUES (1,'DIVISI IT',NULL),(2,'DIVISI FINANCE',NULL),(3,'DIVISI LAIN-LAIN',NULL);
+/*!40000 ALTER TABLE `t_divisi` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `t_pegawai`
+--
+
 DROP TABLE IF EXISTS `t_pegawai`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `t_pegawai` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nik` varchar(50) DEFAULT NULL,
@@ -174,17 +174,25 @@ CREATE TABLE `t_pegawai` (
   KEY `id_divisi` (`id_divisi`),
   CONSTRAINT `id_divisi` FOREIGN KEY (`id_divisi`) REFERENCES `t_divisi` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of t_pegawai
--- ----------------------------
-INSERT INTO `t_pegawai` VALUES ('1', 'TPMM1211002', 'ATWIS ANDREAS HADI SAPUTRO', '1', 'Laki-Laki', 'Jakarta Timur', '085xxxxx', 'aan', '0cc175b9c0f1b6a831c399e269772661', '2019-11-15 22:42:56', null, null, '1', '0', '1');
-INSERT INTO `t_pegawai` VALUES ('2', 'TPMM0415018', 'NANIK ERNAWATI', '3', 'Perempuan', 'Depok', '085xxxxx', 'nanik', '0cc175b9c0f1b6a831c399e269772661', '2019-11-17 18:40:32', null, null, '1', '0', '2');
+--
+-- Dumping data for table `t_pegawai`
+--
 
--- ----------------------------
--- Table structure for t_request
--- ----------------------------
+LOCK TABLES `t_pegawai` WRITE;
+/*!40000 ALTER TABLE `t_pegawai` DISABLE KEYS */;
+INSERT INTO `t_pegawai` VALUES (1,'TPMM1211002','ATWIS ANDREAS HADI SAPUTRO',1,'Laki-Laki','Jakarta Timur','085xxxxx','aan','0cc175b9c0f1b6a831c399e269772661','2019-11-15 22:42:56',NULL,NULL,1,0,1),(2,'TPMM0415018','NANIK ERNAWATI',3,'Perempuan','Depok','085xxxxx','nanik','0cc175b9c0f1b6a831c399e269772661','2019-11-17 18:40:32',NULL,NULL,1,0,2);
+/*!40000 ALTER TABLE `t_pegawai` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `t_request`
+--
+
 DROP TABLE IF EXISTS `t_request`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `t_request` (
   `request_id` int(11) NOT NULL AUTO_INCREMENT,
   `kode_permintaan` varchar(100) DEFAULT NULL,
@@ -195,28 +203,82 @@ CREATE TABLE `t_request` (
   `keterangan_bayar` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`request_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of t_request
--- ----------------------------
+--
+-- Dumping data for table `t_request`
+--
 
--- ----------------------------
--- Table structure for t_role
--- ----------------------------
+LOCK TABLES `t_request` WRITE;
+/*!40000 ALTER TABLE `t_request` DISABLE KEYS */;
+/*!40000 ALTER TABLE `t_request` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `t_role`
+--
+
 DROP TABLE IF EXISTS `t_role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `t_role` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nama` varchar(50) DEFAULT '',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of t_role
--- ----------------------------
-INSERT INTO `t_role` VALUES ('1', 'ADMINISTRATOR');
-INSERT INTO `t_role` VALUES ('2', 'DIREKTUR');
-INSERT INTO `t_role` VALUES ('3', 'KEPALA IT');
-INSERT INTO `t_role` VALUES ('4', 'KEPALA FINANCE');
-INSERT INTO `t_role` VALUES ('5', 'IT');
-INSERT INTO `t_role` VALUES ('6', 'HRD');
-INSERT INTO `t_role` VALUES ('7', 'FINANCE');
+--
+-- Dumping data for table `t_role`
+--
+
+LOCK TABLES `t_role` WRITE;
+/*!40000 ALTER TABLE `t_role` DISABLE KEYS */;
+INSERT INTO `t_role` VALUES (1,'ADMINISTRATOR'),(2,'DIREKTUR'),(3,'KEPALA IT'),(4,'KEPALA FINANCE'),(5,'IT'),(6,'HRD'),(7,'FINANCE');
+/*!40000 ALTER TABLE `t_role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `temp_barang`
+--
+
+DROP TABLE IF EXISTS `temp_barang`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `temp_barang` (
+  `id` int(11) NOT NULL,
+  `session_id` int(11) DEFAULT NULL,
+  `kategori` int(11) DEFAULT NULL,
+  `merk` varchar(255) DEFAULT NULL,
+  `suplier` int(11) DEFAULT NULL,
+  `nama_barang` varchar(100) DEFAULT NULL,
+  `harga_satuan` float(11,0) DEFAULT NULL,
+  `qty` int(11) DEFAULT NULL,
+  `keterangan` text DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `temp_barang`
+--
+
+LOCK TABLES `temp_barang` WRITE;
+/*!40000 ALTER TABLE `temp_barang` DISABLE KEYS */;
+/*!40000 ALTER TABLE `temp_barang` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'sitpmm'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-11-24  2:36:28
